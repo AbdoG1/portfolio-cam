accdbd3c59c8a1ed006239b88716f71d0e3e3b40
AbdoG1 <140276519+AbdoG1@users.noreply.github.com>
2026-06-10 18:47:24 +0300

    Add new HTML pages for exhibitions, about, contact, and projects; implement JavaScript for navbar transparency; enhance CSS for styling and layout

---
+++ index.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    if($(this).scrollTop() >= 200) {
+        $('#navbar').addClass('noTransparent');
+    } else {
+        $('#navbar').removeClass('noTransparent');
+    }
+})
+
+
+
